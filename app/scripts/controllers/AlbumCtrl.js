1518d953c791318954db99df4adc3a56e7ec8bf2
Jacob Laurenti <jlaurenti@Jacobs-MBP.home>
2017-10-08 11:32:22 -0400

    Assignment 6 Update

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,10 +1,6 @@
 (function() {
     function AlbumCtrl(Fixtures) {
       this.albumData = Fixtures.getAlbum();
-      this.albumSongs = [];
-      for (var i=0; i < this.albumData.songs.length; i++) {
-        this.albumSongs.push(this.albumData.songs[i]);
-}
     }
 
     angular
