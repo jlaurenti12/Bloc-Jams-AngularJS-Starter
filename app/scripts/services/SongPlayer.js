90a45f2d5e7c600427014f5a08b9b7898e8b5c09
Jacob Laurenti <jlaurenti@Jacobs-MBP.home>
2017-10-10 13:09:26 -0400

    Assignment 10 Complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -164,6 +164,13 @@
           }
         };
 
+        SongPlayer.volume = null;
+
+        SongPlayer.setVolume = function(volume) {
+          if (currentBuzzObject) {
+            currentBuzzObject.setVolume(volume);
+          }
+        };
 
 
          return SongPlayer;
